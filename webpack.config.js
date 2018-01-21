4f5579d507f15f96c0a023bb8bb755eaed307353
Rolands Jegorovs <rolandjegorov@gmail.com>
2018-01-21 13:08:21 +0200

    Remove redundant dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,44 @@
 const path = require('path');
-// const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-// const ExtractTextWebpack = require('extract-text-webpack-plugin')
+// var UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+// var ExtractTextWebpack = require('extract-text-webpack-plugin')
 
-module.exports = env => {
-    return {
-        devtool: "inline-source-map",
-        entry: "./src/index.tsx",
-        output: {
-            path: path.resolve(__dirname, "public"),
-            filename: "build/app.js"
-        },
-        resolve: {
-            extensions: [".ts", ".tsx", ".js", ".json", ".css"],
-        },
-        node: {
-            fs: "empty",
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.tsx?$/,
-                    exclude: path.resolve(__dirname, "node_modules"),
-                    loader: "ts-loader",
-                },
-                {
-                    test: /\.css$/,
-                    loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]' 
-                },
-                {
-                    test: /\.(png|jp(e*)g|svg)$/,  
-                    use: [{
-                        loader: 'url-loader',
-                        options: { 
-                            limit: 8000, // Convert images < 8kb to base64 strings
-                            name: 'images/[hash]-[name].[ext]'
-                        } 
-                    }]
-                }
-            ],
-        },
-        // plugins: [
-        //     new UglifyJsPlugin({
-        //         include:  path.resolve(__dirname, "src"),
-        //         exclude:  path.resolve(__dirname, "node_modules"),
-        //     }),
-        // ],
-    }
-}
+module.exports = env => ({
+    devtool: "inline-source-map",
+    entry: "./src/index.tsx",
+    output: {
+        path: path.resolve(__dirname, "public"),
+        filename: "build/app.js"
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".json", ".css"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: path.resolve(__dirname, "node_modules"),
+                loader: "ts-loader",
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]' 
+            },
+            {
+                test: /\.(png|jp(e*)g|svg)$/,  
+                use: [{
+                    loader: 'url-loader',
+                    options: { 
+                        limit: 8000, // Convert images < 8kb to base64 strings
+                        name: 'images/[hash]-[name].[ext]'
+                    } 
+                }]
+            }
+        ],
+    },
+    // plugins: [
+    //     new UglifyJsPlugin({
+    //         include:  path.resolve(__dirname, "src"),
+    //         exclude:  path.resolve(__dirname, "node_modules"),
+    //     }),
+    // ],
+});
