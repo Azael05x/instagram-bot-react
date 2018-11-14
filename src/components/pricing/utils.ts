2bb83d230d3bd64a71c3051a65f3859a4f8d4c1f
Rolands Jegorovs <rolandjegorov@gmail.com>
2018-11-14 16:28:34 +0100

    Fix price plan numbers

--- src/components/pricing/utils.ts
+++ src/components/pricing/utils.ts
@@ -1,5 +1,13 @@
 import { round } from "@utils/numbers";
 
 export function adjustPrice(price: number, multiplier: number, decimals = 4) {
-    return round(price * multiplier, decimals);
+    const priceAdjusted = round(price * multiplier, decimals);
+    const delimiterIndex = priceAdjusted.indexOf(".");
+    const basePrice = priceAdjusted.slice(0, delimiterIndex) + priceAdjusted.slice(delimiterIndex, delimiterIndex + 3);
+    const leftOver = priceAdjusted.replace(basePrice, "");
+    const finalPrice = leftOver === "00"
+        ? basePrice
+        : `${basePrice}${leftOver}`;
+
+    return finalPrice;
 }
